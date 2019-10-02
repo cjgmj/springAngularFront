699d77b384604fd62996e9953be02f4ff6082ded
cjgmj <cjgmj@hotmail.com>
2019-10-02 20:25:50 +0200

    Mover nav al header

--- clientes-app/src/app/header/header.component.ts
+++ clientes-app/src/app/header/header.component.ts
@@ -2,8 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `<h1>Angular - Spring </h1>`
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  title = 'Springular';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
