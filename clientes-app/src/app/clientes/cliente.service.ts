2bef1c2c1566de8bc938308d0e3764e2c917a83b
cjgmj <cjgmj@hotmail.com>
2019-10-05 14:20:00 +0200

    Servicio con HttpClient

--- clientes-app/src/app/clientes/cliente.service.ts
+++ clientes-app/src/app/clientes/cliente.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    return this.http.get<Cliente[]>(`${this.urlEndPoint}/api/clientes`);
   }
 }
