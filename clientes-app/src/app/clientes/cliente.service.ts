b8a2671ac885327b826512513a94e02109cce809
cjgmj <cjgmj@hotmail.com>
2019-10-06 14:55:04 +0200

    Crear cliente

--- clientes-app/src/app/clientes/cliente.service.ts
+++ clientes-app/src/app/clientes/cliente.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { CLIENTES } from './clientes.json';
-import { of, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable()
@@ -10,11 +9,17 @@ export class ClienteService {
 
   private urlEndPoint = 'http://localhost:8080';
 
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get(`${this.urlEndPoint}/api/clientes`).pipe(
       map(response => response as Cliente[])
     );
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(`${this.urlEndPoint}/api/clientes`, cliente, {headers: this.httpHeaders});
   }
 }
