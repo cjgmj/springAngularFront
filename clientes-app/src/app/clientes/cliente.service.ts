c62a804fecc29899b28f78a1c81ad20a638c8977
cjgmj <cjgmj@hotmail.com>
2019-10-06 16:07:56 +0200

    Eliminar cliente

--- clientes-app/src/app/clientes/cliente.service.ts
+++ clientes-app/src/app/clientes/cliente.service.ts
@@ -30,4 +30,8 @@ export class ClienteService {
   update(cliente: Cliente): Observable<Cliente> {
     return this.http.put<Cliente>(`${this.urlEndPoint}/api/clientes/${cliente.id}`, cliente, {headers: this.httpHeaders});
   }
+
+  delete(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/api/clientes/${id}`, {headers: this.httpHeaders});
+  }
 }
--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -15,6 +16,36 @@ export class ClientesComponent implements OnInit {
 
   ngOnInit() {
     this.clienteService.getClientes().subscribe( clientes => this.clientes = clientes );
+  }
+
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success mx-1',
+        cancelButton: 'btn btn-danger mx-1'
+      },
+      buttonsStyling: false
+    });
+
+    swalWithBootstrapButtons.fire({
+      title: `¿Estás seguro que desea eliminar al cliente ${cliente.nombre}?`,
+      text: 'No podrás deshacerlo',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe( resp => {
+          this.clientes = this.clientes.filter(cli => cli !== cliente);
+          swalWithBootstrapButtons.fire(
+            'Cliente eliminado',
+            `El cliente ${cliente.nombre} eliminado con éxito`,
+            'success'
+          );
+        });
+      }
+    });
   }
 
 }
