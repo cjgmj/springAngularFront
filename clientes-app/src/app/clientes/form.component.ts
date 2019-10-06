86f0f416c440fb35cc5cee3a96f92bd1fd601796
cjgmj <cjgmj@hotmail.com>
2019-10-06 15:07:32 +0200

    Instalar SweetAlert2

--- clientes-app/src/app/clientes/form.component.ts
+++ clientes-app/src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -21,6 +22,7 @@ export class FormComponent implements OnInit {
   public create(): void {
     this.clienteService.create(this.cliente).subscribe(response => {
         this.router.navigate(['/clientes']);
+        swal.fire('Cliente guardado', `El cliente ${response.nombre} se ha creado con éxito`, 'success');
       }
     );
   }
